3798326312bf539975722997fcfa4afb5c9c09a2
leticea <nascimento.leticeah@gmail.com>
2023-03-14 16:41:39 -0300

    criando função no js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function toggleMode() {
   const html = document.documentElement;
+  html.classList.toggle("light");
+
+  const img = document.querySelector("#profile img");
 
   if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png");
+    img.setAttribute("alt", "Foto de Letícia com cabelo curto na altura do queixo, usando óculos escuros e sorrindo");
+  } else {
+    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute("alt", "Foto de Letícia usando no rosto um efeito do Coringa e vestindo uma blusa azul da seleção brasileira");
   }
 }
